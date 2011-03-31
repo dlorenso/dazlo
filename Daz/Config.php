2c40c35de796de35af76af87b20766948549628c
Dante Lorenso <dante@lorenso.com>
2011-03-30 20:45:45 -0500

    added copyright and license statements to all classes -- Dante

--- Daz/Config.php
+++ Daz/Config.php
@@ -4,9 +4,10 @@ class Daz_Config {
      * Dazlo Framework
      * Copyright (c) 2011 D. Dante Lorenso.  All Rights Reserved.
      *
-     * This source file is subject to the new BSD license that is bundled with
-     * this package in the file LICENSE.txt.  It is also available through the
-     * world-wide- web.
+     * This source file is subject to the new BSD license that is bundled
+     * with this package in the file LICENSE.txt.  It is also available
+     * through the world-wide web at this URL:
+     * http://www.opensource.org/licenses/bsd-license.php
      */
 
     //----------------------------------------------------------------------
--- Daz/Form/Args.php
+++ Daz/Form/Args.php
@@ -1,3 +1,14 @@
 <?php
 class Daz_Form_Args {
+    /**
+     * Dazlo Framework
+     * Copyright (c) 2011 D. Dante Lorenso.  All Rights Reserved.
+     *
+     * This source file is subject to the new BSD license that is bundled
+     * with this package in the file LICENSE.txt.  It is also available
+     * through the world-wide web at this URL:
+     * http://www.opensource.org/licenses/bsd-license.php
+     */
+
+    //----------------------------------------------------------------------
 }
