f9d5f524eacbad9c5c26f64495da81b26af8451e
dlorenso <dante@lorenso.com>
2011-05-12 23:43:19 -0700

    added ability to fetch $_REQUEST vars as string and boolean

--- Daz/Request.php
+++ Daz/Request.php
@@ -11,4 +11,42 @@ class Daz_Request {
      */
 
     //----------------------------------------------------------------------
+    /**
+     * Fetch and return an element from the Request array and return it.  If it
+     * is not set, use the default value.
+     */
+    public static function get($key, $default = false) {
+        return isset ($_REQUEST[$key]) ? $_REQUEST[$key] : $default;
+    }
+
+    //----------------------------------------------------------------------
+    /**
+     * Fetch an element from the Request array and return it as a boolean value.
+     */
+    public static function getbool($key, $default = false) {
+        $v = self :: get($key, $default);
+
+        // we can only test scalar values
+        if (!is_scalar($v)) {
+            return (boolean) $v;
+        }
+
+        // case-insensitive checking
+        $v = strtolower($v);
+
+        // clearly it is TRUE
+        if ($v == 'yes' || $v == 'true' || $v == 't' || $v == 'on' || $v === true) {
+            return true;
+        }
+
+        // clearly it is FALSE
+        if ($v == 'no' || $v == 'false' || $v == 'f' || $v == 'off' || $v === false) {
+            return false;
+        }
+
+        // its not clear, just cast it and be done with it
+        return (boolean) $v;
+    }
+
+    //----------------------------------------------------------------------
 }
