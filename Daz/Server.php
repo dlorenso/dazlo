4bad2f2394cc769d3263ca1388c84684cc26b654
Dante Lorenso <larkspark@menchib.dreamhost.com>
2011-06-11 02:55:30 -0700

    added comments to help guide the blind and weak

--- Daz/Server.php
+++ Daz/Server.php
@@ -11,11 +11,21 @@ class Daz_Server {
      */
 
     //----------------------------------------------------------------------
+    /**
+     * Safely fetch a value from the PHP superglobal $_SERVER without throwing a
+     * PHP warning about undefined keys.  If the key does not exist or is empty,
+     * we will return a default value of our choosing or false otherwise.
+     */
     public static function get($key, $default = null) {
         return empty ($_SERVER[$key]) ? $default : $_SERVER[$key];
     }
 
     //----------------------------------------------------------------------
+    /**
+     * This should never be needed but was created to perform the opposite
+     * function of a 'get'.  On some environments, it may be necessary to
+     * override system defaults but setting $_SERVER values manually.
+     */
     public static function set($key, $value) {
         $_SERVER[$key] = $value;
     }
